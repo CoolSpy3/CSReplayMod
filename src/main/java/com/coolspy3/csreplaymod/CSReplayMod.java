f5817037ffac02a123f3352fd67b2a21de23a85e
CoolSpy3 <coolspythree@gmail.com>
2021-12-11 01:23:26 -0800

    fix version

--- src/main/java/com/coolspy3/csreplaymod/CSReplayMod.java
+++ src/main/java/com/coolspy3/csreplaymod/CSReplayMod.java
@@ -9,7 +9,7 @@ import net.hypixel.api.reply.StatusReply;
 @Mod(id = "replaymod", name = "CSReplayMod",
         description = "Provides the ability to easily replay game types", version = "2.0.0",
         dependencies = {"csmodloader:[1,2)", "cspackets:[1,2)", "csutils:[1,2)",
-                "cshypixelapi:[1.1,2)"})
+                "cshypixelapi:[2,3)"})
 public class CSReplayMod implements Entrypoint
 {
 
